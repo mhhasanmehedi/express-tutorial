602849f1381b17527ddc9d8a604304b14cea8a13
mhhasanmehedi <mmmehedi53@gmail.com>
2023-01-14 20:10:05 +0600

    Request Object

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 const express = require("express");
+const cookieParser = require("cookie-parser");
+const handler = require("./handler");
 
 const app = express();
+app.use(express.json());
+app.use(cookieParser());
 
-app.enable("case sensitive routing");
+const adminRoute = express.Router();
 
-app.all("/about", (req, res) => {
-  res.send("This is home page");
+adminRoute.get("/dashboard", (req, res) => {
+  console.log(req.ip);
+  res.send("We are in admin dashboard");
+});
+
+app.use("/admin", adminRoute);
+
+app.get("/user/:id", handler);
+
+app.post("/user", (req, res) => {
+  console.log(req.route);
+  res.send("Hello World post");
 });
 
 app.listen(4000, () => {
--- readme.md
+++ readme.md
@@ -1,3 +1,3 @@
-https://www.youtube.com/watch?v=5q3NeKhhLQ0&list=PLHiZ4m8vCp9PHnOIT7gd30PCBoYCpGoQM&index=17&ab_channel=LearnwithSumit-LWS-Bangladesh
+https://www.youtube.com/watch?v=gPp65W7tnFM&list=PLHiZ4m8vCp9PHnOIT7gd30PCBoYCpGoQM&index=18&ab_channel=LearnwithSumit-LWS-Bangladesh
 
-11:40 / 25:58
+15:57 / 39:37
